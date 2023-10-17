2fbe5beb37a0c2491f7dd25d9067e34eac484693
Luke Rieff <luke.rieff@gmail.com>
2023-10-17 14:21:40 +0200

    add config support

--- src/main.py
+++ src/main.py
@@ -32,7 +32,7 @@ class MyStatusWindow(QtWidgets.QDialog):
 
         # Creates the elapsed time label.
         self.elapsed_time_label = QtWidgets.QLabel()
-        self.elapsed_time_label.setText('Elapsed time: -')
+        self.elapsed_time_label.setText('Verlopen tijd: -')
         self.grid.addWidget(self.elapsed_time_label, 2, 0)
 
         # Creates the button box.
@@ -62,7 +62,7 @@ class MyStatusWindow(QtWidgets.QDialog):
     def on_timer_timeout(self):
         current_time = time()
         elapsed_time = int(round((current_time - self.start_time) * 100.0) * 10.0)
-        self.elapsed_time_label.setText(f'Elapsed time: {elapsed_time}ms')
+        self.elapsed_time_label.setText(f'Verlopen tijd: {elapsed_time}ms')
 
     @QtCore.Slot()
     def on_log(self, log: ConverterLogEvent) -> None:
@@ -85,28 +85,42 @@ class MyStatusWindow(QtWidgets.QDialog):
 
 
 class MyWindowWidget(QtWidgets.QWidget):
-    def __init__(self) -> None:
+    def __init__(self, settings: QtCore.QSettings) -> None:
         super().__init__()
 
-        drawings_path = Path.home() / 'Werktekeningen'
-        self.in_dir_path = drawings_path / 'Import'
-        self.out_dir_path = drawings_path / 'Archief'
-        self.fail_dir_path = drawings_path / 'Handmatig'
-        self.finished_dir_path = drawings_path / 'Verwerkt'
-        self.temp_dir_path = drawings_path / 'Temp'
+        self.settings = settings
+
+        # Gets the paths where the files will be located.
+
+        self.default_drawings_path = Path.home() / 'Werktekeningen'
+        self.in_dir_path = Path(str(self.settings.value('folders/in', str(self.default_drawings_path / 'Import'))))
+        self.out_dir_path = Path(str(self.settings.value('folders/out', str(self.default_drawings_path / 'Archief'))))
+        self.fail_dir_path = Path(str(self.settings.value('folders/fail', str(self.default_drawings_path / 'Handmatig'))))
+        self.finished_dir_path = Path(str(self.settings.value('folders/finished', str(self.default_drawings_path / 'Verwerkt'))))
+        self.temp_dir_path = Path(str(self.settings.value('folders/temp', str(self.default_drawings_path / 'Temp'))))
+
+        # Ensures the paths where the files will be located exist.
+
+        self.in_dir_path.mkdir(parents=True, exist_ok=True)
+        self.out_dir_path.mkdir(parents=True, exist_ok=True)
+        self.fail_dir_path.mkdir(parents=True, exist_ok=True)
+        self.finished_dir_path.mkdir(parents=True, exist_ok=True)
+        self.temp_dir_path.mkdir(parents=True, exist_ok=True)
+
+        # Creates the grid.
 
         self.grid = QtWidgets.QGridLayout(self)
 
         # Title
 
         self.title_label = QtWidgets.QLabel()
-        self.title_label.setText('DX OCR Drawing Processor')
+        self.title_label.setText('DX Drawing OCR')
         self.grid.addWidget(self.title_label, 0, 0)
 
         # Group box
 
         self.dirs_group_box = QtWidgets.QGroupBox()
-        self.dirs_group_box.setTitle('Directories')
+        self.dirs_group_box.setTitle('Mappen')
 
         # Directories layout
 
@@ -116,9 +130,13 @@ class MyWindowWidget(QtWidgets.QWidget):
 
         # Input directory
 
-        self.select_in_dir_btn = QtWidgets.QPushButton('Select input folder')
+        self.select_in_dir_btn = QtWidgets.QPushButton('Kies nieuwe map')
         self.select_in_dir_btn.clicked.connect(self.on_select_in_dir_btn_clicked)
         self.dirs_layout.addWidget(self.select_in_dir_btn, 0, 0)
+
+        self.view_in_dir_btn = QtWidgets.QPushButton('Bekijk map')
+        self.view_in_dir_btn.clicked.connect(self.on_view_in_dir_btn_clicked)
+        self.dirs_layout.addWidget(self.view_in_dir_btn, 0, 2)
 
         self.in_dir_txt_ln_edit = QtWidgets.QLineEdit()
         self.in_dir_txt_ln_edit.setReadOnly(True)
@@ -128,9 +146,13 @@ class MyWindowWidget(QtWidgets.QWidget):
 
         # Output directory
 
-        self.select_out_dir_btn = QtWidgets.QPushButton('Select output folder')
+        self.select_out_dir_btn = QtWidgets.QPushButton('Kies nieuwe map')
         self.select_out_dir_btn.clicked.connect(self.on_select_out_dir_btn_clicked)
         self.dirs_layout.addWidget(self.select_out_dir_btn, 1, 0)
+
+        self.view_out_dir_btn = QtWidgets.QPushButton('Bekijk map')
+        self.view_out_dir_btn.clicked.connect(self.on_view_out_dir_btn_clicked)
+        self.dirs_layout.addWidget(self.view_out_dir_btn, 1, 2)
 
         self.out_dir_txt_ln_edit = QtWidgets.QLineEdit()
         self.out_dir_txt_ln_edit.setReadOnly(True)
@@ -140,9 +162,13 @@ class MyWindowWidget(QtWidgets.QWidget):
 
         # Failure directory
 
-        self.select_fail_dir_btn = QtWidgets.QPushButton('Select failure folder')
+        self.select_fail_dir_btn = QtWidgets.QPushButton('Kies nieuwe map')
         self.select_fail_dir_btn.clicked.connect(self.on_select_fail_dir_btn_clicked)
         self.dirs_layout.addWidget(self.select_fail_dir_btn, 2, 0)
+
+        self.view_fail_dir_btn = QtWidgets.QPushButton('Bekijk map')
+        self.view_fail_dir_btn.clicked.connect(self.on_view_fail_dir_btn_clicked)
+        self.dirs_layout.addWidget(self.view_fail_dir_btn, 2, 2)
 
         self.fail_dir_txt_ln_edit = QtWidgets.QLineEdit()
         self.fail_dir_txt_ln_edit.setReadOnly(True)
@@ -152,9 +178,13 @@ class MyWindowWidget(QtWidgets.QWidget):
 
         # Finished directory
 
-        self.select_finished_dir_btn = QtWidgets.QPushButton('Select Finished folder')
+        self.select_finished_dir_btn = QtWidgets.QPushButton('Kies nieuwe map')
         self.select_finished_dir_btn.clicked.connect(self.on_select_finished_dir_btn_clicked)
         self.dirs_layout.addWidget(self.select_finished_dir_btn, 3, 0)
+
+        self.view_finished_dir_btn = QtWidgets.QPushButton('Bekijk map')
+        self.view_finished_dir_btn.clicked.connect(self.on_view_finished_dir_btn_clicked)
+        self.dirs_layout.addWidget(self.view_finished_dir_btn, 3, 3)
 
         self.finished_dir_txt_ln_edit = QtWidgets.QLineEdit()
         self.finished_dir_txt_ln_edit.setReadOnly(True)
@@ -162,19 +192,36 @@ class MyWindowWidget(QtWidgets.QWidget):
         self.finished_dir_txt_ln_edit.setText(str(self.finished_dir_path))
         self.dirs_layout.addWidget(self.finished_dir_txt_ln_edit, 3, 1)
 
-        self.clear_finished_dir_btn = QtWidgets.QPushButton('Clear finished folder')
+        self.clear_finished_dir_btn = QtWidgets.QPushButton('Wis')
         self.clear_finished_dir_btn.clicked.connect(self.on_clear_finished_folder_btn_clicked)
         self.dirs_layout.addWidget(self.clear_finished_dir_btn, 3, 2)
 
         # Process button
 
-        self.process_btn = QtWidgets.QPushButton('Process')
+        self.process_btn = QtWidgets.QPushButton('Verwerk')
         self.process_btn.clicked.connect(self.on_process_btn_clicked)
         self.grid.addWidget(self.process_btn, 2, 0)
 
     @QtCore.Slot()
+    def on_view_out_dir_btn_clicked(self) -> None:
+        QtGui.QDesktopServices.openUrl(QtCore.QUrl.fromLocalFile(str(self.out_dir_path)))
+
+    @QtCore.Slot()
+    def on_view_in_dir_btn_clicked(self) -> None:
+        QtGui.QDesktopServices.openUrl(QtCore.QUrl.fromLocalFile(str(self.in_dir_path)))
+
+    @QtCore.Slot()
+    def on_view_fail_dir_btn_clicked(self) -> None:
+        QtGui.QDesktopServices.openUrl(QtCore.QUrl.fromLocalFile(str(self.fail_dir_path)))
+
+    @QtCore.Slot()
+    def on_view_finished_dir_btn_clicked(self) -> None:
+        QtGui.QDesktopServices.openUrl(QtCore.QUrl.fromLocalFile(str(self.finished_dir_path)))
+
+    @QtCore.Slot()
     def on_process_btn_clicked(self) -> None:
-        converter = Converter.build(self.in_dir_path, self.out_dir_path, self.fail_dir_path, self.temp_dir_path, self.finished_dir_path)
+        converter = Converter.build(self.in_dir_path, self.out_dir_path, self.fail_dir_path, self.temp_dir_path,
+                                    self.finished_dir_path)
         my_status_window = MyStatusWindow(converter)
         my_status_window.exec()
 
@@ -186,6 +233,7 @@ class MyWindowWidget(QtWidgets.QWidget):
 
         self.in_dir_path = Path(new_in_dir_path)
         self.in_dir_txt_ln_edit.setText(str(self.in_dir_path))
+        self.settings.setValue('folders/in', str(new_in_dir_path))
 
     @QtCore.Slot()
     def on_select_out_dir_btn_clicked(self) -> None:
@@ -195,6 +243,7 @@ class MyWindowWidget(QtWidgets.QWidget):
 
         self.out_dir_path = Path(new_out_dir_path)
         self.out_dir_txt_ln_edit.setText(str(self.out_dir_path))
+        self.settings.setValue('folders/out', str(new_out_dir_path))
 
     @QtCore.Slot()
     def on_select_fail_dir_btn_clicked(self) -> None:
@@ -204,6 +253,7 @@ class MyWindowWidget(QtWidgets.QWidget):
 
         self.fail_dir_path = Path(new_fail_dir_path)
         self.fail_dir_txt_ln_edit.setText(str(self.fail_dir_path))
+        self.settings.setValue('folders/fail', str(new_fail_dir_path))
 
     @QtCore.Slot()
     def on_select_finished_dir_btn_clicked(self) -> None:
@@ -213,6 +263,7 @@ class MyWindowWidget(QtWidgets.QWidget):
 
         self.finished_dir_path = Path(new_finished_dir_path)
         self.finished_dir_txt_ln_edit.setText(str(self.finished_dir_path))
+        self.settings.setValue('folders/finished', str(new_finished_dir_path))
 
     @QtCore.Slot()
     def on_clear_finished_folder_btn_clicked(self) -> None:
@@ -223,9 +274,11 @@ class MyWindowWidget(QtWidgets.QWidget):
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
 
+    settings = QtCore.QSettings("DuflexMechatronics", "DrawingOCR")
+
     app = QtWidgets.QApplication()
 
-    my_window_widget = MyWindowWidget()
+    my_window_widget = MyWindowWidget(settings)
     my_window_widget.show()
 
     sys.exit(app.exec())
